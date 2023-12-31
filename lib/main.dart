1493932bc140b2f992476b1ab0317ec566a64213
Nahidasef <naas50dx@gmail.com>
2023-12-31 22:49:13 +0600

    EmaiL

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:medminder/auth/firstscreen.dart';
 import 'widgets/navbar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:medminder/auth/authListener.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
