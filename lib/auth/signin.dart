ee73197d379bac921f652ae2047a69b6658ccd3d
Nahidasef <naas50dx@gmail.com>
2024-01-13 11:58:14 +0600

    email check

--- lib/auth/signin.dart
+++ lib/auth/signin.dart
@@ -193,8 +193,10 @@ class _Signin extends State<Signin> {
                                     content: Text("$result")));
                               }
 
-                              Navigator.pushNamedAndRemoveUntil(
-                                  context, '/nav', (route) => false);
+                              else{
+                                Navigator.pushNamedAndRemoveUntil(
+                                    context, '/nav', (route) => false);
+                              }
                             } else if (!rememberPassword) {
                               ScaffoldMessenger.of(context).showSnackBar(
                                 const SnackBar(
